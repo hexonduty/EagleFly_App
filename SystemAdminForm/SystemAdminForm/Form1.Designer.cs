edb3fbb17c46445a3aa8926123d5e4b2cf063269
Efecan Açıkgöz <132709251+hexonduty@users.noreply.github.com>
2024-02-27 20:50:26 +0300

    Add files via upload

---
+++ SystemAdminForm/SystemAdminForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SystemAdminForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.userPassword = new System.Windows.Forms.MaskedTextBox();
+            this.usersGroupBox = new System.Windows.Forms.GroupBox();
+            this.usersListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userRepeatPassword = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.usersGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SystemAdminForm.Properties.Resources.User;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.userNameLabel.Location = new System.Drawing.Point(12, 22);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(59, 18);
+            this.userNameLabel.TabIndex = 2;
+            this.userNameLabel.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password";
+            // 
+            // userName
+            // 
+            this.userName.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.userName.Location = new System.Drawing.Point(12, 43);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(159, 27);
+            this.userName.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.loginButton.Location = new System.Drawing.Point(12, 196);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(159, 47);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "Register";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // userPassword
+            // 
+            this.userPassword.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.userPassword.Location = new System.Drawing.Point(15, 103);
+            this.userPassword.Mask = "00000";
+            this.userPassword.Name = "userPassword";
+            this.userPassword.Size = new System.Drawing.Size(159, 27);
+            this.userPassword.TabIndex = 7;
+            this.userPassword.ValidatingType = typeof(int);
+            // 
+            // usersGroupBox
+            // 
+            this.usersGroupBox.Controls.Add(this.usersListBox);
+            this.usersGroupBox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usersGroupBox.Location = new System.Drawing.Point(201, 68);
+            this.usersGroupBox.Name = "usersGroupBox";
+            this.usersGroupBox.Size = new System.Drawing.Size(571, 346);
+            this.usersGroupBox.TabIndex = 8;
+            this.usersGroupBox.TabStop = false;
+            this.usersGroupBox.Text = "Active Users";
+            // 
+            // usersListBox
+            // 
+            this.usersListBox.FormattingEnabled = true;
+            this.usersListBox.ItemHeight = 18;
+            this.usersListBox.Location = new System.Drawing.Point(23, 35);
+            this.usersListBox.Name = "usersListBox";
+            this.usersListBox.Size = new System.Drawing.Size(510, 256);
+            this.usersListBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Repeat Password";
+            // 
+            // userRepeatPassword
+            // 
+            this.userRepeatPassword.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.userRepeatPassword.Location = new System.Drawing.Point(12, 163);
+            this.userRepeatPassword.Mask = "00000";
+            this.userRepeatPassword.Name = "userRepeatPassword";
+            this.userRepeatPassword.Size = new System.Drawing.Size(159, 27);
+            this.userRepeatPassword.TabIndex = 10;
+            this.userRepeatPassword.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userRepeatPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usersGroupBox);
+            this.Controls.Add(this.userPassword);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.usersGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.MaskedTextBox userPassword;
+        private System.Windows.Forms.GroupBox usersGroupBox;
+        private System.Windows.Forms.ListBox usersListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox userRepeatPassword;
+    }
+}
+
---
+++ SystemAdminForm/SystemAdminForm/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemAdminForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Welcome to the Eagle Fly System.\r\n Please click the OK button to load the system.", "İnformation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            usersListBox.Items.Add("einTheDeveloper" + " : " + "unknown");
+        }
+
+
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+            int userPasswordInput = Convert.ToInt32(userPassword.Text);
+            int userRepeatPasswordInput = Convert.ToInt32(userRepeatPassword.Text);
+
+            if (userName.Text.Length <= 10 && userPasswordInput == userRepeatPasswordInput)
+            {
+                MessageBox.Show("Your Username and Password have been save successfully");
+
+                string userData = userName.Text + " : " + userPasswordInput;
+                usersListBox.Items.Add(userData);
+            }
+            else
+            {
+                MessageBox.Show("Your Username must be equal to ten Characters and Your Password must be consistent of only Numbers");
+            }
+        }
+
+       
+    }
+}
